02dc095a3eb587180f4ad48fed3813fd48fd2b51
Isabella-Amantino <isabella.amantino@hotmail.com>
2022-10-19 14:04:01 -0300

    Deploy do Proejto Blog Pessoal

---
+++ src/main/java/com/blogpessoal/blogpessoal/model/Tema.java
@@ -0,0 +1,58 @@
+package com.blogpessoal.blogpessoal.model;
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@Entity
+@Table(name = "tb_temas")
+public class Tema {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@NotNull
+	private String descricao;
+
+	@OneToMany(mappedBy = "tema", cascade = CascadeType.REMOVE)//tirar o ALL e por Remove. Quando apaga o tema, todas as postagens vão apagar  tudo
+	@JsonIgnoreProperties("tema")
+	private List<Postagem> postagem; //Lista de postagem para um tipo de tema. Então, na hora de pesquisar o tema, aparece todas as postagens desse tema
+	
+	
+	
+	public List<Postagem> getPostagem() {
+		return postagem;
+	}
+
+	public void setPostagem(List<Postagem> postagem) {
+		this.postagem = postagem;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	
+	
+}
